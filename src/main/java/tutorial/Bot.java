4883641caa839adebc4ac3f2ca1b34db7fdff937
Mykola Fant <mykola.fant@hazelcast.com>
2023-12-06 22:53:01 +0200

    add env vars

--- src/main/java/tutorial/Bot.java
+++ src/main/java/tutorial/Bot.java
@@ -1,16 +1,56 @@
 package tutorial;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
+    private String token;
+    protected Bot(String token) {
+        this.token = token;
+    }
+
+    private void logMessageData(Message message) {
+        System.out.print("UserId: " + message.getFrom().getId() + "; ");
+        System.out.println("MessageText: " + message.getText());
+    }
     @Override
     public void onUpdateReceived(Update update) {
+        Message msg = update.getMessage();
+        User user = msg.getFrom();
+        String text = msg.getText();
 
+        logMessageData(msg);
+
+        if (msg.isCommand()) {
+            if (text.equals("/start")) {
+                sendText(user.getId(), "Привіт, " + user.getFirstName() + "!");
+            } else if (text.equals("/say_something")) {
+                sendText(user.getId(), "Але шоо ти дууумаєш?..");
+            }
+        } else {
+            sendText(user.getId(), "Але " + text);
+        }
+    }
+
+    private void sendText(Long userId, String message){
+        SendMessage sm = SendMessage.builder()
+                .chatId(userId.toString())
+                .text(message)
+                .build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String getBotToken() {
-        return "4839574812:AAFD39kkdpWt3ywyRZergyOLMaJhac60qc";
+        return this.token;
     }
 
     @Override
--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -1,4 +1,24 @@
 package tutorial;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
+    private static final String tokenKey = "BOT_TOKEN";
+    public static void main(String[] args) throws TelegramApiException  {
+        String token = System.getenv(tokenKey);
+        if (token == null) {
+            token = System.getProperty(tokenKey);
+        }
+        if (token == null) {
+            throw new TelegramApiException("Could not find BOT token...");
+        }
+        launchTelegramBot(token);
+    }
+    private static void launchTelegramBot(String token) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+        botsApi.registerBot(new Bot(token));
+    }
 }
